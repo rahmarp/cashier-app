8413609ccf89e96bccee2b4e3544cd6176f68873
rahmarp_ <rahmarizadyp@gmail.com>
2020-12-22 14:22:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RegistrationPage from './containers/RegistrationPage';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/RegistrationPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class RegistrationPage extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" value="Rahma"/>
+                <input type="text" value="Rahma"/>
+                <button></button>
+            </div>
+        )
+    }
+}
+
+export default RegistrationPage
